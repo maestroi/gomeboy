246c503c90c10276cb1474fafb661209644b77b0
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-26 07:58:39 +0200

    chore: keep PPU timing docs attached to methods

--- internal/gba/ppu/ppu.go
+++ internal/gba/ppu/ppu.go
@@ -129,8 +129,6 @@ func (p *PPU) Reset() {
 	p.bus.SetOBJVRAMStart(0x10000)
 }
 
-// Advance advances LCD timing by master-clock cycles. It may cross any number
-// of scanline/frame boundaries.
 // CyclesUntilEvent returns the number of master-clock cycles until the next
 // PPU timing edge that can change externally visible state or emit a hook.
 func (p *PPU) CyclesUntilEvent() uint32 {
@@ -144,6 +142,8 @@ func (p *PPU) CyclesUntilEvent() uint32 {
 	}
 }
 
+// Advance advances LCD timing by master-clock cycles. It may cross any number
+// of scanline/frame boundaries.
 func (p *PPU) Advance(cycles uint32) {
 	for cycles > 0 {
 		next := CyclesPerLine
